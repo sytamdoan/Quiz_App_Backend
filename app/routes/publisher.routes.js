b4280ba48d315b77abaf1d2980de4e17ee4c51e8
An Nguyen <annguyen3132@gmail.com>
2025-06-08 16:26:19 -0500

    Create routes for publisher

--- app/routes/publisher.routes.js
+++ app/routes/publisher.routes.js
@@ -2,8 +2,21 @@ module.exports = (app) => {
   const Publisher = require("../controllers/publisher.controller.js");
   const { authenticateRoute } = require("../authentication/authentication.js");
   var router = require("express").Router();
-  // Retrieve all published Genres
+
+  // Retrieve all Publishers
   router.get("/Publishers", Publisher.findAll);
 
+  // Retrieve one Publisher
+  router.get("/Publisher/:id", Publisher.findOne);
+
+  // Create a Publisher
+  router.post("/Publisher", Publisher.create);
+
+  // Update a Publisher
+  router.put("/Publisher/:id", Publisher.update);
+
+  // Delete a Publisher
+  router.delete("/Publisher/:id", Publisher.delete);
+  
   app.use("/bookshelfapi", router);
 };
