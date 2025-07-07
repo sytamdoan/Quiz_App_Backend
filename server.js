ce6473ed1ad0e39e5f3b80742768c894a966dcf2
Tam Doan <sytamdoan@gmail.com>
2025-07-07 15:46:06 -0500

    testing

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.get("/", (req, res) => {
 });
 
 //require("./app/routes/auth.routes.js")(app);
+//testing
 ;//require("./app/routes/author.routes")(app);
 require("./app/routes/user.routes")(app);
 require("./app/routes/LLM.routes")(app);
