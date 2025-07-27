ed2b2d93ce357923426925e9199b960eb88aaf3f
An nguyen <annguyen3132@gmail.com>
2025-07-27 14:49:08 -0500

    Add unique constraints to prevent multiple answers for being registered to the same quizSession/question/user

--- app/models/response.model.js
+++ app/models/response.model.js
@@ -1,6 +1,30 @@
+const { DataTypes } = require("sequelize");
+
 module.exports = (sequelize, Sequelize) => {
   const Quiz = sequelize.define("response", {
-    
+    quizSessionId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    questionId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    answerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+  }, {
+    indexes: [
+      {
+        unique: true,
+        fields: ['quizSessionId', 'questionId', 'userId']
+      }
+    ]
   });
 
   return Quiz;
