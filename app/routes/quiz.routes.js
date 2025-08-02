e149f2bd684860995e1ca578c6d5918475b46519
joshua.meek7@gmail.com <joshua.meek7@gmail.com>
2025-08-02 17:57:57 -0500

    First commit of locking down quiz once a session is started from it

--- app/routes/quiz.routes.js
+++ app/routes/quiz.routes.js
@@ -5,20 +5,17 @@ module.exports = (app) => {
   // Create a new Quiz
   router.post("/Class/:id/Quiz/", Quiz.create);
 
-  // Create a new Quiz as a duplicate of one
+  // Create a new Quiz as a duplicate of another one
   router.post("/Quiz/:id/duplicate", Quiz.duplicateQuiz);
 
   // Retrieve all Quiz relating to this Class
   router.get("/Class/:id/Quiz/", Quiz.findAll);
 
-  // Retrieve a Quiz by a Question iD
-  router.get("/Quiz/:questionId/Question/", Quiz.findOneByQuestionId);
-
-  // Retrieve a Quiz by an iD
-  router.get("/Quiz/:id", Quiz.findOneById);
-
   // Update a Quiz with id
   router.put("/Quiz/:id", Quiz.update);
+
+  // Update a Quiz with id but only one specific piece of the quiz
+  router.put("/Quiz/:id/lock", Quiz.lockQuiz);
 
   // Delete a Quiz with id
   router.delete("/Quiz/:id", Quiz.delete);
